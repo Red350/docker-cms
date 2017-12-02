d65640c2c2c30141eb924da6b9da10b62781ef12
Padraig Redmond <pwredmond@gmail.com>
2017-12-02 18:43:43 +0000

    Added tests for adding, tagging, and viewing images

--- cmstests.py
+++ cmstests.py
@@ -2,27 +2,34 @@ import json
 from subprocess import Popen, PIPE
 
 base_url = "http://localhost:80"
-get_request= "-X GET"
+get_request = "-X GET"
+post_request = "-X POST"
+patch_request = "-X PATCH"
+delete_request = "-X DELETE"
 accept_json = "-H 'Accept: application/json'"
 base_curl_command = ['curl', '-s', accept_json]
 
+image_tag = "sshd"
+
 def print_test_header(header):
     num_symbols = len(header) + 4
-    print("#" * num_symbols)
+    print("\n" +"#" * num_symbols)
     print("# " + header + " #")
-    print("#" * num_symbols)
+    print("#" * num_symbols + "\n")
 
 def print_command(args):
     print("Executing command...")
-    print("curl -s" + " ".join(args))
+    print(" ".join(base_curl_command) + " ".join(args))
 
 def print_output(output):
-    print(json.dumps(json.loads(output), indent=4, sort_keys=True))
+    print(json.dumps(output, indent=4, sort_keys=True))
 
+# Makes a curl request with the supplied args
 def curl(*args):
-    cmd = base_curl_command
+    cmd = base_curl_command[:]
     for sub in args:
         cmd.append(sub)
+    print(cmd)
     process = Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     stdout_decoded = stdout.decode("utf-8")
@@ -31,40 +38,61 @@ def curl(*args):
         print('Error: {0} -> {1}'.format(' '.join(cmd), stderr_decoded))
     return stderr_decoded + stdout_decoded
 
+# Prints the output of a curl request as json, and returns that json
+def process_command(args):
+    output = json.loads(curl(*args))
+    print_output(output)
+    return output
+
 def list_containers():
     args = (get_request, base_url + "/containers")
     print_command(args)
     print("Listing containers...")
-    print_output(curl(*args))
+    return process_command(args)
 
 def list_running_containers():
     args = (get_request, base_url + "/containers?=running")
     print_command(args)
     print("Listing running images...")
-    print_output(curl(*args))
+    return process_command(args)
 
 def list_images():
-    args = ("-s", "-X", "GET", "-H", "'Accept: application/json'", "http://{}:80/images".format(ip))
+    args = (get_request, base_url + "/images")
     print_command(args)
     print("Listing images...")
-    print_output(curl(*args))
+    return process_command(args)
+
+def add_image(filepath):
+    args = (post_request, "-F", "file=@{}".format(filepath), base_url + "/images")
+    print_command(args)
+    print("Creating image for {}...".format(filepath))
+    return process_command(args)
+
+def tag_image(id, tag):
+    tag_json = json.dumps({"tag": tag})
+    args = (patch_request, base_url + "/images/{}".format(id), "-d", tag_json)
+    print_command(args)
+    print("Updating tag for id: {} to {}...".format(id, tag))
+    return process_command(args)
 
 
 # Image tests
 
-# Test 1: Add an image
+# Test 1: Create an image
 def test_add_image():
-    list_containers()
-#list_images()
+    print_test_header("Test 1: Create an image")
+    new_image = add_image("dockerfiles/sshd.Dockerfile")
+    return new_image["id"]
 
-# Test 2: List all images
+# Test 2: Update image tag
+def test_update_image(image_id):
+    print_test_header("Test 2: Update image tag")
+    tag_image(image_id, image_tag)
 
-# Test 3: Update image tag
-
-# Test 4: Delete an image
-
-# Test 5: Force delete all images
-
+# Test 3: List all images
+def test_list_images():
+    print_test_header("Test 3: List all images")
+    list_images()
 
 # Container tests
 
@@ -82,15 +110,30 @@ def test_add_image():
 
 # Test 12: Restart container
 
+
+
+
+# Deletion tests
+
 # Test 13: Delete a container
 
-# Test 14: Delete all containers
+# Test 14: Force delete all containers
+
+# Test 4: Delete an image
+
+# Test 5: Force delete all images
 
 
-# Swarm tests
+# Docker swarm tests
 
 # Test 15: List all services
 
 # Test 16: List all nodes in the swarm
 
-test_add_image()
+# Call all the tests
+#test_add_image()
+#test_list_images()
+#test_update_image()
+
+#list_containers()
+#list_images()
